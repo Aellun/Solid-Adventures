7ba0e591474a942ac140c95ad35179a08cd959d4
Aellun <okeelo.k.otieno@gmail.com>
2023-11-23 05:29:35 +0300

    operational application

--- task-manager/task_mgt_GUI.py
+++ task-manager/task_mgt_GUI.py
@@ -4,102 +4,150 @@ import sqlite3 as sql
 
 def main():
     root = tk.Tk()
-    root.title("TASK MANAGEMENT APPLICATION")  # the window title
+    root.title("TASK MANAGEMENT APPLICATION")
+    root.resizable(0,0)
+    root.geometry("800x600+750+250")
+
+    header_frame = tk.Frame(root, bg="#FAEBD7")
+    functions_frame = tk.Frame(root, bg="#FAEBD7")
+    functions_frame.pack(side='left', expand=True, fill='both')
+    header_frame.pack(side ='top',fill='both')
     
-    # the size of window on display
-    root.geometry("500x700")
-    # creating a label-- this is like header of the page and styling it
-    label = tk.Label(root, text="My do To List", font=('Courier', 18))
-    label.pack(padx=28, pady=28)
-    label.config(fg="#0000FF")
-    label.config(bg="Antique white")
+    listbox_frame = tk.Frame(root, bg="#FAEBD7")
+    listbox_frame.pack(side='right', expand=True, fill='both')
 
+    header_label = tk.Label(
+        header_frame,
+        text="My To-Do List",
+        font=("Helvetica", "25"),
+        background="#FAEBD7",
+        foreground="#B9D9EB"
+    )
+    header_label.pack(padx=6, pady=40)
 
-    # Entry widget for adding tasks
-    enter_task = tk.Label(root, text="Enter your task below", font=('Courier', 16))
-    enter_task.pack(padx=10, pady=10)
-    entry = tk.Entry(root, width=40)
-    entry.pack(pady=20)
+    task_label = tk.Label(
+        functions_frame,
+        text="Enter the Task:",
+        font=("Consolas", "11", "bold"),
+        background="#FAEBD7",
+        foreground="#000000"
+    )
+    task_label.place(x=30, y=60)
 
-    # Listbox for displaying tasks
-    tasks[]
-    tasks = tk.Listbox(root, selectmode=tk.SINGLE, width=40, height=10)
-    tasks.pack()
+    task_entry = tk.Entry(
+        functions_frame,
+        font=("Consolas", "12"),
+        width=24,
+        background="#FFF8DC",
+        foreground="#A52A2A"
+    )
+    task_entry.place(x=30, y=80)
 
-    # Buttons for adding and deleting tasks
-    add_button = tk.Button(root, text="Add Task", command=lambda: add_task(entry, tasks))
-    add_button.pack(pady=5)
+    tasks = []
 
-    delete_button = tk.Button(root, text="Delete Task", command=lambda: delete_task(tasks))
-    delete_button.pack(pady=5)
+    # The database connection
+    the_connection = sql.connect('listOfTasks.db')
+    the_cursor = the_connection.cursor()
+    the_cursor.execute('create table if not exists tasks (title text)')
+
+    def add_task():
+        task_string = task_entry.get()
+        if len(task_string) == 0:
+            messagebox.showinfo('Error', 'Field Empty.')
+        else:
+            tasks.append(task_string)
+            the_cursor.execute('insert into tasks values(?)', (task_string,))
+            the_connection.commit()
+            list_update()
+            task_entry.delete(0,'end')
+
+    def list_update():
+        clear_list()
+        for task in tasks:
+            task_listbox.insert('end', task)
+
+    def delete_task():
+        try:
+            the_value = task_listbox.get(task_listbox.curselection())
+            if the_value in tasks:
+                tasks.remove(the_value)
+                list_update()
+                the_cursor.execute('delete from tasks where title = ?', (the_value,))
+                the_connection.commit()
+        except IndexError:
+            messagebox.showwarning("Warning", "Please select a task to delete.")
+
+    def delete_all_tasks():
+        message_box = messagebox.askyesno('Delete all', 'Are you sure?')
+        if message_box == True:
+            while (len(tasks) != 0):
+                tasks.pop()
+            the_cursor.execute('delete from tasks')
+            the_connection.commit()
+            list_update()
+
+    def clear_list():
+        task_listbox.delete(0, 'end')
+
+    def close():
+        print(tasks)
+        root.destroy()
+
+    def retrieve_database():
+        while (len(tasks) != 0):
+            tasks.pop()
+        for row in the_cursor.execute('select title from tasks'):
+            tasks.append(row[0])
+
+    add_button = tk.Button(
+        functions_frame,
+        text="Add Task",
+        width=24,
+        command=add_task
+    )
+    del_button = tk.Button(
+        functions_frame,
+        text="Delete Task",
+        width=24,
+        command=delete_task
+    )
+    del_all_button = tk.Button(
+        functions_frame,
+        text="Delete All Tasks",
+        width=24,
+        command=delete_all_tasks
+    )
+    exit_button = tk.Button(
+        functions_frame,
+        text="Exit",
+        width=24,
+        command=close
+    )
+    add_button.place(x=30, y=120)
+    del_button.place(x=30, y=160)
+    del_all_button.place(x=30, y=200)
+    exit_button.place(x=30, y=240)
+
+    task_listbox = tk.Listbox(
+        listbox_frame,
+        width=26,
+        height=13,
+        selectmode='SINGLE',
+        background="#FFFFFF",
+        foreground="#000000",
+        selectbackground="#CD853F",
+        selectforeground="#FFFFFF"
+    )
+    task_listbox.place(x=10, y=20)
+
+    retrieve_database()
+    list_update()
+
+    the_connection.commit()
+    
 
     root.mainloop()
-
-def add_task():
-    #getting the string from the entry field
-    task_string = task_field.get()
-    #check if the string is empty or not
-    if len(task_string) == 0:
-    #display an error message if the string is empty
-        messagebox > showinfo('Error field Empty.')
-    else:
-        #adding the string to the task list
-        task.append(task_string)
-        #the cursor execute method to excecute a sql statement
-        the_cursor.execute('insert into tasks values(?)', (task_string,))
-        list_update()
-        task_field.delete(0, 'end')# deleting the entry in the entry field
-        
-        #listbox.insert(tk.END, task)
-        #entry.delete(0, tk.END)  # Clear the entry widget
-    #else:
-        #messagebox.showwarning("Warning", "Please enter a task.")
-def list_update():
-    '''This function will update the list'''
-    clear_list()  # calling the function to clear the list
-    for task in tasks:
-        task_listbox.insert('end',task)
-
-
-def delete_task():
-    ''' the delete_task function deletes the selected task from the
-    list and database'''
-    try:
-        the_value = task_listbox.get(listbox > cursorselection())
-        if the_value in tasks:
-            tasks.remove(the_value)
-            list_update()
-            the_cursor.execute('delete from the tasks where title =?',(the_value))
-        #selected_task_index = listbox.curselection()[0]
-        #listbox.delete(selected_task_index)
-    except IndexError:
-        messagebox.showwarning("Warning", "Please select a task to delete.")
-
-def delete_all_tasks():
-    ''' function to delete all tasks from the list'''
-    message_box = messagebox.askyesno('Delete all', 'Are you sure?')
-    if message_box == = True:
-        while (len(tasks) != 0):# Use the loop to iterate thru' the tasks list until its empty
-            tasks.pop()  # Pop up the element from the list
-        the_cursor.execute('delete from tasks')# Execute the sql statement
-        list_update()
-
-def clear_list():
-    ''' The function to clear the list using the delete method'''
-    task_listbox.delete(0,'end')
-
-def close():
-    ''' Function to close the application but prints the task before closing'''
-    print(tasks)
-    guiWindow.destroy()
-
-def retrieve_database():
-    ''' this is a function to retieve data from the database'''
-    while (len(tasks) != 0):
-        tasks.pop()
-    for row in the_cursor.execute('select title from tasks'):
-        tasks.append(row[0])# method to insert the titles from the table to the list
-
+    the_cursor.close()
 
 if __name__ == "__main__":
     main()
