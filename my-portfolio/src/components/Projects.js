e0e1eb5992daa131345ea4e6c236e7dabb4ac713
Aellun <okeelo.k.otieno@gmail.com>
2023-12-11 17:12:55 +0300

    updated the project component

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -25,29 +25,29 @@ const Projects = () => {
                 <h3><a href="https://github.com/Aellun/Solid-Adventures/tree/main/mycommand-console" target="_blank">Console</a></h3>
                 <p>Description:<br />
                     Built with Python<br />
-                    This is console that mimics the functionality of terminal in Linux or CMD in Windows
-                    The commands for both windows are fused together, meaning Window and Linux users can
-                    use it easily with the same command.</p>
+                    This is console that mimics the functionality of terminal on Linux or CMD on Windows
+                    The commands for both windows are fused together, meaning you don't have to be
+                    a Window and Linux user to easily use it
+                </p>
             </div>
             <div>
                 <h3><a href="https://github.com/Aellun/Solid-Adventures/tree/main/calculator" target="_blank">Calculator</a></h3>
                 <p>Description:<br />
                     Built with Python Tkinter Library<br />
                     This is just a simple calculator which the user can download and use on the desktop
-                    for those who can not the pyinstaller it has an executable  which you can download and start using
-                    immediately</p>
+                </p>
             </div>
             <div>
                 <h3><a href="https://github.com/Aellun/Solid-Adventures/tree/main/my-portfolio" target="_blank">Portfolio</a></h3>
                 <p>Description:<br />
                     Built with Javascript,React,css<br />
-                    Simpke web- based application that display my skills</p>
+                    Simple web- based application that display my skills</p>
             </div>
             <h3><a href='https://github.com/Aellun/Solid-Adventures/tree/main/app_logging' target='_blank'>Logs</a></h3>
             <p>Description:<br />
                 built with Python Tkinter library and SQLite3<br />
-                This is graphical user interface PC logs that stores the all the application and servises
-                running in the host pc as long as it is set to run. user can review and analyze such logs easilyby choosing the
+                This is graphical user interface PC logs that stores the all the application and servise logs
+                As long as it is running in the host pc it will store the logs. user can review and analyze such logs easilyby choosing the
                 the dates</p>
         </div>
 
--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -6,11 +6,53 @@ const Skills = () => {
     return (
         <div className="container-content">
             <h2>Skills</h2>
-            <ul>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Node.js</li>
-                {/* Add more skills as needed */}
+            <ul className="skills-list">
+                <li>
+                    <span className="skill-name">Python</span>
+                    <p className="sub-note">Dynamic Libraries: Tkinter.</p>
+                    <p className="sub-note">Object-Oriented Programming (OOP).</p>
+                    <p className="sub-note">Data Structures and Collections.</p>
+                </li>
+                <li>
+                    <span className="skill-name">HTML/5 CSS</span>
+                    <p className="sub-note">Web Development Fundamentals.</p>
+                    <p className="sub-note">CSS Styling and Frameworks.</p>
+                </li>
+                <li>
+                    <span className="skill-name">C</span>
+                    <p className="sub-note">Pointer and Memory Management</p>
+                    <p className="sub-note">Data Structures.</p>
+                </li>
+
+                <li>
+                    <span className="skill-name">React and Node.js</span>
+                    <p className="sub-note">Front-End Development with React.</p>
+                    <p className="sub-note">Back-End Development with Node.js.</p>
+                </li>
+                <li>
+                    <span className="skill-name">Bash/Shell Scripting</span>
+                    <p className="sub-note">Automation Solutions</p>
+                    <p className="sub-note"> System Administration</p>
+                    <p className="sub-note">Workflow Optimization:</p>
+                </li>
+                <li>
+                    <span className="skill-name">Networking</span>
+                    <p className="sub-note">Network Infrastructure Management.</p>
+                    <p className="sub-note">Network Troubleshooting and Maintenance.</p>
+                </li>
+                <li>
+                    <span className="skill-name">Database Management</span>
+                    <p className="sub-note">Database Administration.</p>
+                    <p className="sub-note"> Data Security and Integrity.</p>
+                    <p className="sub-note"> Database Design and Implementation.</p>
+                </li>
+                <li>
+                    <span className="skill-name">Remote Desktop Tools</span>
+                    <p className="sub-note">Remote Desktop Administration.</p>
+                    <p className="sub-note">Troubleshooting and Support.</p>
+                    <p className="sub-note">Security and Access Control.</p>
+                </li>
+                {/* Add more skills with sub-notes as needed */}
             </ul>
         </div>
     );
